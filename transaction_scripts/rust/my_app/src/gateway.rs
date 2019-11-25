5199a46603fedf8accdb3a28f5500a015c2bc87b
Gahan Rakholia <gahan94rakh@gmail.com>
2019-11-25 22:51:20 +0530

    [gahan] add find_contract to gateway

--- transaction_scripts/rust/my_app/src/gateway.rs
+++ transaction_scripts/rust/my_app/src/gateway.rs
@@ -17,9 +17,18 @@ impl Gateway {
         stmt.query(&[&contract_id, &as_of])
     }
 
+    pub fn find_contract(&self, contract_id: i64) -> Result<Rows, Error> {
+        let stmt = self.find_contract_statement()?;
+        stmt.query(&[&contract_id])
+    }
+
     fn find_recognition_statement(&self) -> postgres::Result<Statement<'_>> {
         self.conn
             .prepare("SELECT * FROM revenueRecognition WHERE contract = $1 AND recognizedOn = $2")
+    }
+
+    fn find_contract_statement(&self) -> postgres::Result<Statement<'_>> {
+        self.conn.prepare("SELECT * FROM contracts, product WHERE contracts.id = $1 AND product = product.id")
     }
 }
 
@@ -29,15 +38,27 @@ mod tests {
     use chrono::NaiveDate;
     use postgres::TlsMode;
 
-    #[test]
-    fn test_find_recognition_statement() {
+    fn setup() -> Gateway {
         let conn = Connection::connect(
             "postgresql://postgres@localhost:5432/poea_rs",
             TlsMode::None,
         ).unwrap();
-        let gateway = Gateway::new(conn);
+        Gateway::new(conn)
+    }
+
+    #[test]
+    fn test_find_recognition_statement() {
+        let gateway = setup();
         let date = NaiveDate::from_ymd(2019, 3, 10);
         let result = gateway.find_recognition_for(200, date).unwrap();
         assert_eq!(result.len(), 0);
     }
+
+    #[test]
+    fn test_find_contract_statement() {
+        let gateway = setup();
+        let result = gateway.find_contract(200).unwrap();
+        assert_eq!(result.len(), 0);
+    }
+
 }
