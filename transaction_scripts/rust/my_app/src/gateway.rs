568eb3538d8c39019a381588f1c890226b90a7ad
Gahan Rakholia <gahan94rakh@gmail.com>
2019-11-27 01:06:47 +0530

    [gahan] calculate and save revenueRecognition

--- transaction_scripts/rust/my_app/src/gateway.rs
+++ transaction_scripts/rust/my_app/src/gateway.rs
@@ -2,33 +2,35 @@ use postgres::rows::Rows;
 use postgres::stmt::Statement;
 use postgres::{Connection, Error};
 
+type Money = i64;
+
 #[derive(Debug)]
 pub struct Gateway {
     conn: Connection,
 }
 
 impl Gateway {
+    const FIND_RECOGNITION_STATEMENT: &'static str = "SELECT * FROM revenueRecognition WHERE contract = $1 AND recognizedOn = $2";
+    const FIND_CONTRACT_STATEMENT: &'static str = "SELECT * FROM contracts, product WHERE contracts.id = $1 AND product = product.id";
+    const INSERT_RECOGNITION_STATEMENT: &'static str = "INSERT INTO revenueRecognition VALUES($1,$2,$3)";
+
     pub fn new(conn: Connection) -> Gateway {
         Gateway { conn }
     }
 
     pub fn find_recognition_for(&self, contract_id: i64, as_of: chrono::NaiveDate) -> Result<Rows, Error> {
-        let stmt = self.find_recognition_statement()?;
+        let stmt = self.conn.prepare(Gateway::FIND_RECOGNITION_STATEMENT)?;
         stmt.query(&[&contract_id, &as_of])
     }
 
     pub fn find_contract(&self, contract_id: i64) -> Result<Rows, Error> {
-        let stmt = self.find_contract_statement()?;
+        let stmt = self.conn.prepare(Gateway::FIND_CONTRACT_STATEMENT)?;
         stmt.query(&[&contract_id])
     }
 
-    fn find_recognition_statement(&self) -> postgres::Result<Statement<'_>> {
-        self.conn
-            .prepare("SELECT * FROM revenueRecognition WHERE contract = $1 AND recognizedOn = $2")
-    }
-
-    fn find_contract_statement(&self) -> postgres::Result<Statement<'_>> {
-        self.conn.prepare("SELECT * FROM contracts, product WHERE contracts.id = $1 AND product = product.id")
+    pub fn insert_recognition(&self, contract_id: i64, amount: Money, as_of: chrono::NaiveDate) -> postgres::Result<u64> {
+        let stmt = self.conn.prepare(Gateway::INSERT_RECOGNITION_STATEMENT)?;
+        stmt.execute(&[&contract_id, &amount, &as_of])
     }
 }
 
@@ -61,4 +63,11 @@ mod tests {
         assert_eq!(result.len(), 0);
     }
 
+    #[test]
+    fn test_insert_recognition() {
+        let gateway = setup();
+        let date = NaiveDate::from_ymd(2019, 3, 10);
+        let result = gateway.insert_recognition(20, 300, date);
+        assert_eq!(result.unwrap(), 1);
+    }
 }
--- transaction_scripts/rust/my_app/src/service/recognition_service.rs
+++ transaction_scripts/rust/my_app/src/service/recognition_service.rs
@@ -1,27 +1,65 @@
 use crate::gateway::Gateway;
 use chrono::NaiveDate;
-
-#[derive(Debug)]
-pub struct RecognitionService {
-    gateway: Gateway
-}
+use std::ops::Add;
+use time::Duration;
 
 type Money = i64;
 
+#[derive(Debug)]
+pub struct RecognitionService {
+    db: Gateway
+}
+
 impl RecognitionService {
-    pub fn new(gateway: Gateway) -> RecognitionService {
+    pub fn new(db: Gateway) -> RecognitionService {
         RecognitionService {
-            gateway
+            db
         }
     }
 
     pub fn recognized_revenue(&self, contract_num: i64, as_of: NaiveDate) -> Money {
-        let rows = self.gateway.find_recognition_for(contract_num, as_of).unwrap();
+        let rows = self.db.find_recognition_for(contract_num, as_of).unwrap();
 
         rows.iter().map(|row| {
             let amount: i64 = row.get(1);
             amount
         }).sum()
+    }
+
+    pub fn calculate_revenue_recognitions(&self, contract_num: i64) {
+        // TODO: change it to use `?` symbol
+        let contracts = self.db.find_contract(contract_num).unwrap();
+        for row in contracts.iter() {
+            let total_revenue: Money = row.get(2);
+            let recognition_date: chrono::NaiveDate = row.get(3);
+            let product_type: i64 = row.get(1);
+            if product_type == 1 {
+                // product_type == Spreadsheet
+                let allocations = self.allocate(total_revenue, 3);
+                self.db.insert_recognition(contract_num, allocations[0], recognition_date);
+                self.db.insert_recognition(contract_num, allocations[1], recognition_date.add(Duration::days(60)));
+                self.db.insert_recognition(contract_num, allocations[2], recognition_date.add(Duration::days(90)));
+            } else if product_type == 2 {
+                // product_type == Word Processor
+                self.db.insert_recognition(contract_num, total_revenue, recognition_date);
+            } else if product_type == 3 {
+                // product_type == database
+                let allocations = self.allocate(total_revenue, 3);
+                self.db.insert_recognition(contract_num, allocations[0], recognition_date);
+                self.db.insert_recognition(contract_num, allocations[1], recognition_date.add(Duration::days(30)));
+                self.db.insert_recognition(contract_num, allocations[2], recognition_date.add(Duration::days(60)));
+            }
+        }
+    }
+
+    fn allocate(&self, money: Money, split: i64) -> Vec<Money> {
+        let split = money / split;
+        let mut money_bag: Vec<i64> = Vec::new();
+        for i in 0..split {
+            money_bag.push(split);
+        }
+
+        money_bag
     }
 }
 
@@ -33,12 +71,22 @@ mod tests {
     #[test]
     fn test_recognized_revenue() {
         let conn = Connection::connect("postgresql://postgres@localhost:5432/poea_rs", TlsMode::None).unwrap();
-        let gateway = Gateway::new(conn);
+        let db = Gateway::new(conn);
+        let service = RecognitionService::new(db);
 
-        let service = RecognitionService::new(gateway);
         let date = NaiveDate::from_ymd(2019, 3, 10);
         let result = service.recognized_revenue(23400, date);
 
         assert_eq!(result, 0);
+    }
+
+    #[test]
+    fn test_calculate_revenue_recognitions() {
+        let conn = Connection::connect("postgresql://postgres@localhost:5432/poea_rs", TlsMode::None).unwrap();
+        let db = Gateway::new(conn);
+        let service = RecognitionService::new(db);
+
+        service.calculate_revenue_recognitions(200);
+        assert_eq!(1, 1);
     }
 }
